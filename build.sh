ca478ace5385671b98e0b64692b1485a5c653092
Nathan <nathan@njsdomain.net>
2026-09-14 16:16:00 -0400

    builder

--- build.sh
+++ build.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+set -a
+source "$(dirname "$0")/.env"
+set +a
+
+if [ -z "$IMAGE" ] || [ -z "$CONTAINER_CMD" ]; then
+    echo "IMAGE or CONTAINER_CMD is not set - check .env" >&2
+    exit 1
+fi
+
 VER=`mvn help:evaluate -Dexpression=project.version -q -DforceStdout`
-echo "*** Building version $VER ***"
+echo "*** Building $IMAGE version $VER ***"
 rm -rf target
 mvn clean install
-docker build --build-arg VERSION=$VER -t containeryard.evoforge.org/gmdev/platform/cortana-backend:$VER .
+$CONTAINER_CMD build --build-arg VERSION=$VER -t $IMAGE:$VER .
--- push-cy.sh
+++ push-cy.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+set -a
+source "$(dirname "$0")/.env"
+set +a
+
+if [ -z "$IMAGE" ] || [ -z "$CONTAINER_CMD" ]; then
+    echo "IMAGE or CONTAINER_CMD is not set - check .env" >&2
+    exit 1
+fi
+
 VER=`mvn help:evaluate -Dexpression=project.version -q -DforceStdout`
-echo "*** Pushing version $VER ***"
-docker push containeryard.evoforge.org/gmdev/platform/cortana-backend:$VER
+echo "*** Pushing $IMAGE version $VER ***"
+$CONTAINER_CMD push $IMAGE:$VER
